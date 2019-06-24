72ed8bd5518baf42b3bddc1aae40665f0bf653a5
bibesh07 <bibesh.kc@selu.edu>
2019-06-24 17:01:28 -0500

    updated the appId and masterKey

--- index.js
+++ index.js
@@ -14,8 +14,8 @@ if (!databaseUri) {
 var api = new ParseServer({
   databaseURI: databaseUri || 'mongodb+srv://admin:pr1mt3kP@$$@cluster0-vresh.mongodb.net/test?retryWrites=true&w=majority',
   cloud: process.env.CLOUD_CODE_MAIN || __dirname + '/cloud/main.js',
-  appId: process.env.APP_ID || 'myAppId',
-  masterKey: process.env.MASTER_KEY || 'myMasterId', //Add your master key here. Keep it secret!
+  appId: 'snapIce',
+  masterKey: 'primtekId', //Add your master key here. Keep it secret!
   serverURL: process.env.SERVER_URL || 'http://localhost:1337/parse',  // Don't forget to change to https if needed
   liveQuery: {
     classNames: ["Posts", "Comments"] // List of classes to support for query subscriptions
