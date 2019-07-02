db4b6c4e7cbed125f90c5d878ee9dc5f4a55687c
bibesh07 <bibesh.kc@selu.edu>
2019-07-02 16:08:41 -0500

    Added a new index page
    rendering html instead of a plain text

--- index.js
+++ index.js
@@ -39,7 +39,7 @@ app.use(mountPath, api);
 
 // Parse Server plays nicely with the rest of your web routes
 app.get('/', function(req, res) {
-  res.status(200).send('I dream of being a website.  Please star the parse-server repo on GitHub!');
+  res.sendFile(__dirname + '/index.html');
 });
 
 // There will be a test page available on the /test path of your server url
